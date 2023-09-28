b4a626977e99a79647a295a0d09b792f17153a7f
OEszter <oraveczne.eszter@gmail.com>
2023-09-28 09:39:58 +0200

    gyakorlás: for / array

---
+++ practicesForMyself.js
@@ -0,0 +1,15 @@
+let arrayOfColors = ["fehér", "fekete", "szürke", "kék", "barna", "zöld", "piros", "sárga", "narancs", "lila", "rózsaszín", "lazacrózsaszín"];
+
+//console.log(arrayOfColors.length);
+
+for (let index = 0; index < arrayOfColors.length; index++) {
+    if (arrayOfColors[index].includes('í')) {
+        console.log(`A ${arrayOfColors[index]} tartalmaz 'í' betűt.`)
+        } else {
+            console.log(`A ${arrayOfColors[index]} nem tartalmaz 'í' betűt.`)
+        }
+}
+
+for (let index = 0; index < arrayOfColors.length; index++) {
+    
+}
