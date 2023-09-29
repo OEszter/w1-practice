83029f43fe3993a0a7a92fbbf1e774fe0091e41d
OEszter <oraveczne.eszter@gmail.com>
2023-09-29 11:42:17 +0200

    objektumok

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-console.log("hello world")
+/*console.log("hello world")
 
 for (let index = 0; index <= 5; index++) {
     console.log(index);
-};
+};*/
+
+
+let x = 10;
+let y = "text";
+let z = true
+let arr = ["one", "two", "three", "four"];
+let arr2 = [10, 5, 67, 234];
+let arr3 = ["text", 67, false, null]
+
+let newArr = [arr, arr2, arr3];
+
+console.log(newArr[0][0]);
